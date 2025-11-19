102d15e4dbc8994e5d28fd0b0fc70038acded861
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-19 15:40:37 +0100

    ajout de methodes a Director

--- src/fr/fms/school/Director.java
+++ src/fr/fms/school/Director.java
@@ -5,12 +5,16 @@ import java.util.List;
 
 public class Director extends Person {
     private List<Course> courses;
+    private List<Teacher> teachers;
+    private List<Student> students;
 
     public Director(String firstName, String lastName, int age, Address address) {
         super(firstName, lastName, age, address);
         this.courses = new ArrayList<>();
+        this.teachers = new ArrayList<>();
+        this.students = new ArrayList<>();
     }
-
+    /// Pour la gestion des cours
     //ajouter
     public void addCourse(Course course) {
         courses.add(course);
@@ -22,6 +26,40 @@ public class Director extends Person {
     //afficher
     public List<Course> getCourses() {
         return courses;
+    }
+
+    // pour les élèves
+    public void validateStudentRegistration(Student student) {
+        if (!students.contains(student)) {
+            students.add(student);
+            System.out.println("Inscription validée pour : " + student.getFirstName() + " " + student.getLastName());
+        }
+    }
+    public void viewStudentDetails(Student student) {
+        System.out.println(student);
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    // gestion des profs
+    public void addTeacher(Teacher teacher) {
+        teachers.add(teacher);
+    }
+
+    public void updateTeacher(int index, Teacher teacher) {
+        if (index >= 0 && index < teachers.size()) {
+            teachers.set(index, teacher);
+        }
+    }
+
+    public void removeTeacher(Teacher teacher) {
+        teachers.remove(teacher);
+    }
+
+    public List<Teacher> getTeachers() {
+        return teachers;
     }
 
     // affecter un prof
