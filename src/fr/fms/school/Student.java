e16d01e3f0cc2c2b098a68ab7553b580a9698c58
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2025-11-19 15:30:03 +0100

    student class done

--- src/fr/fms/school/Student.java
+++ src/fr/fms/school/Student.java
@@ -1,4 +1,24 @@
 package fr.fms.school;
 
 public class Student {
+    private int id;
+
+    public Student(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                '}';
+    }
 }
