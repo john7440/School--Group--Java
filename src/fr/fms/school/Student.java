a792c5eaa7b6d91647aa377974102cd0c0574781
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-11-21 09:10:22 +0100

    Add association Student -> Course add list of course on Student add getters and setters add function to add a new course add function to print it with a phrase

--- src/fr/fms/school/Student.java
+++ src/fr/fms/school/Student.java
@@ -1,12 +1,17 @@
 package fr.fms.school;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student extends Person {
     private static int nextid = 1;
     private int id;
+    private List<Course> assignedCourses;
 
 
     public Student(String firstName, String lastName, int age, Address address) {
         super(firstName, lastName, age, address);
+        assignedCourses = new ArrayList<>();
         this.id = nextid;
         nextid++;
     }
@@ -19,7 +24,15 @@ public class Student extends Person {
         this.id = id;
     }
 
-    //Demande d'inscription (que le directeur doit valider)
+    public List<Course> getAssignedCourses() {
+		return assignedCourses;
+	}
+
+	public void setAssignedCourses(List<Course> assignedCourses) {
+		this.assignedCourses = assignedCourses;
+	}
+
+	//Demande d'inscription (que le directeur doit valider)
     public void applyToSchool(){
         Director.getWaitingToBeSubscribedStudents().add(this);
     }
@@ -30,5 +43,20 @@ public class Student extends Person {
         return "\n===Student n°"+ id + "===" +
                 super.toString() +
                 '\n';
+    }
+
+    
+    
+    
+	public void addCourse(Course course) {
+		// TODO Auto-generated method stub
+		assignedCourses.add(course);
+	}
+	
+    //afficher la liste des cours de ce eleve
+    public void printCourses() {
+    	for(Course course : this.assignedCourses) {
+    		System.out.println(course.getName() + " dans la salle " + course.getClassroom() + " avec le formateur " + course.getTeacher().getFirstName());
+    	}
     }
 }
