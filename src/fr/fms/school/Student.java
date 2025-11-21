57ba1ecc9723af6122f49d87aa3edfe19fc1e85b
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-21 09:38:37 +0100

    corrections + améliorations mineures

--- src/fr/fms/school/Student.java
+++ src/fr/fms/school/Student.java
@@ -4,37 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Student extends Person {
-    private static int nextid = 1;
+    private static int nextId = 1;
     private int id;
-    private List<Course> assignedCourses;
+    private final List<Course> assignedCourses;
 
-
+    // ==== Constructeur ====
     public Student(String firstName, String lastName, int age, Address address) {
         super(firstName, lastName, age, address);
         assignedCourses = new ArrayList<>();
-        this.id = nextid;
-        nextid++;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+        this.id = nextId;
+        nextId++;
     }
 
     public List<Course> getAssignedCourses() {
 		return assignedCourses;
 	}
-
-	public void setAssignedCourses(List<Course> assignedCourses) {
-		this.assignedCourses = assignedCourses;
-	}
-
+    // ===Les méthodes====
 	//Demande d'inscription (que le directeur doit valider)
     public void applyToSchool(){
         Director.getWaitingToBeSubscribedStudents().add(this);
+    }
+
+    public void addCourse(Course course) {
+        assignedCourses.add(course);
+    }
+
+    //afficher la liste des cours pour cet élève
+    public void printCourses() {
+        for(Course course : this.assignedCourses) {
+            System.out.println(course.getName() + " dans la salle " + course.getClassroom() + " avec le formateur " + course.getTeacher().getFirstName());
+        }
     }
 
     //Affichage basique de l'élève
@@ -43,20 +42,5 @@ public class Student extends Person {
         return "\n===Student n°"+ id + "===" +
                 super.toString() +
                 '\n';
-    }
-
-    
-    
-    
-	public void addCourse(Course course) {
-		// TODO Auto-generated method stub
-		assignedCourses.add(course);
-	}
-	
-    //afficher la liste des cours de ce eleve
-    public void printCourses() {
-    	for(Course course : this.assignedCourses) {
-    		System.out.println(course.getName() + " dans la salle " + course.getClassroom() + " avec le formateur " + course.getTeacher().getFirstName());
-    	}
     }
 }
